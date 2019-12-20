df95185ea52deef7d05b8e9aed90d0e96a875ace
邵天爽 <shixi_tianshuang@staff.weibo.com>
2019-12-20 17:06:46 +0800

    初次提交

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <settime></settime>
+  </div>
+</template>
+
+<script>
+import settime from "./components/Settime.vue";
+
+export default {
+  name: "app",
+  components: {
+    settime
+  }
+};
+</script>
